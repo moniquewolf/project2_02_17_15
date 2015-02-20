d2d8e6353ec689d8e9a0385c83ee26794037bc56
lorint/brain_fog_blog <monique.n.wolf@gmail.com>
2015-02-19 16:16:43 -0800

    everything else from yesterday

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,22 +4,27 @@ class PostsController < ApplicationController
 	end
 	
 	def new
-  	end
+    @post = Post.new(post_params)
+      if@post.save
+        redirect_to @post
+      else
+        render 'new'
+      end
+  end
 
-  	def create
-  		@post = Post.new(post_params)
-  		@post.save
+	def create
+		@post = Post.new(post_params)
+		@post.save
 
-  		redirect_to @post
-  	end
+		redirect_to @post
+	end
 
-  	def show
-  		@post = Post.find(params[:id])
-  	end
+	def show
+		@post = Post.find(params[:id])
+	end
 
-  	private
-  	def post_params
-  		params.require(:post).permit(:title, :body)
-  	end
-
+	private
+	def post_params
+		params.require(:post).permit(:title, :body)
+	end
 end
