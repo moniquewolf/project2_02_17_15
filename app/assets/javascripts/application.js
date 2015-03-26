65126d87aea856bb8913bb0f0b1d4ed619bba0f0
lorint/brain_fog_blog <monique.n.wolf@gmail.com>
2015-03-25 18:16:00 -0700

    add photos

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,30 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+
+(function($){
+
+	// Defining our jQuery plugin
+
+	$.fn.paulund_modal_box = function(prop){
+
+		// Default parameters
+
+		var options = $.extend({
+			height : "250",
+			width : "500",
+			title:"JQuery Modal Box Demo",
+			description: "Example of how to create a modal box.",
+			top: "20%",
+			left: "30%",
+		},prop);
+				
+		return this.click(function(e){
+		     //Do stuff here
+		});
+				 		
+		return this;
+	};
+	
+})(jQuery);
