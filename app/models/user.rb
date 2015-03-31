81c841689cf11de2430fd0b68da2af54a643624e
lorint/brain_fog_blog <monique.n.wolf@gmail.com>
2015-03-30 18:35:45 -0700

    closing bracket

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,6 @@ class User < ActiveRecord::Base
 
 	 :default_url => "/images/:style/missing.png"
 
-	 validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+	 validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
 
 end
