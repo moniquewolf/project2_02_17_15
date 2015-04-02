ff1efa010ff6a527283cac459172c4208dd1bc61
lorint/brain_fog_blog <monique.n.wolf@gmail.com>
2015-04-01 22:31:12 -0700

    added subs info and image

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   resources :sessions
 
   get '/about', to: 'pages#about'
+  get '/substitutions', to: 'pages#sub', as:"substitutions"
 end
 
   # The priority is based upon order of creation: first created -> highest priority.
--- db/migrate/20150331052000_create_substitutions.rb
+++
@@ -1,8 +0,0 @@
-class CreateSubstitutions < ActiveRecord::Migration
-  def change
-    create_table :substitutions do |t|
-
-      t.timestamps null: false
-    end
-  end
-end
